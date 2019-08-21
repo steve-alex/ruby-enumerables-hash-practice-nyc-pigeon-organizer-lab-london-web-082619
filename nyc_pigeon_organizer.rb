9ac8401cfebf2b79f131120dcef44e68782ecbb8
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-21 12:49:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,6 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, values|
     data[attribute].each do |value, names|
-      p value
       names.each do |name|
         if pigeon_list.has_key?(name)
           pigeon_list[name][attribute].push(value.to_s)
