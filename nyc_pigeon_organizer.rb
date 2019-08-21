44f1bfb521fd52d9ce6d1759bc1e4022d012c2ee
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-21 12:50:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,20 +2,18 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attribute, values|
     data[attribute].each do |value, names|
+      p value
       names.each do |name|
-        if pigeon_list.has_key?(name)
-          pigeon_list[name][attribute].push(value.to_s)
-          p value.to_s
-        else
+        unless pigeon_list.has_key?(name)
           pigeon_list[name] = {
               :color => [],
               :gender => [],
               :lives => []
           }
-          redo
+        end
+        pigeon_list[name][attribute].push(value.to_s)
         end
       end
     end
-  end
   pigeon_list
 end
